8b7751905e4bfbebb9b271563f44a3e75bbaae55
cuongtd <trancuong200495@gmail.com>
2019-08-24 12:46:53 +0700

    version 1.0

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-  var d = document.querySelectorAll('#globalContainer #mainContainer .home_right_column #pagelet_gaming_destination_rhc');
-  if(d.length) {
-    d[0].style.visibility = "hidden";
+  var dom = document.querySelector(`
+  #globalContainer
+    #mainContainer
+      .home_right_column
+        #pagelet_gaming_destination_rhc
+  `);
+  if(dom) {
+    dom.style.visibility = "hidden";
   }
 })
